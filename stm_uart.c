76d683e80e90d256d71913060d097602d689c6e0
CharlesWaterhouse <45199627+CharlesWaterhouse@users.noreply.github.com>
2023-08-22 19:36:00 +0800

    Update stm_uart.c

--- stm_uart.c
+++ stm_uart.c
@@ -2,8 +2,6 @@
 UART_HandleTypeDef g_uart1_handle;
 
 void IinitalUart() {
-  
-  
   __HAL_RCC_GPIOA_CLK_ENABLE();  //enable the clock for this port
   GPIO_InitTypeDef switch_tx_init;
   switch_tx_init.Pin = <pin_id>;           //which pin for output, it is tx or rx base on the pysical pin position(datasheet will present this pin is for tx or rx)
@@ -21,7 +19,6 @@ void IinitalUart() {
   switch_rx_init.Mode = GPIO_AF_PP;
   switch_rx_init.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(<port_id>, &switch_rx_init);
-  
   
   __HAL_RCC_USART1_CLK_ENABLE(); //enable the clock for this uart module
   UART_HandleTypeDef uart1_handle;
@@ -43,36 +40,58 @@ void IinitalUart() {
   HAL_UARTEx_SetRxFifoThreshold(&uart1_handle, UART_RXFIFO_THRESHOLD_1_8);
   HAL_UARTEx_DisableFifoMode(&uart1_handle);
   
-  
   __HAL_UART_ENABLE_IT(&uart1_handle, UART_IT_RXNE); //connect this uart module to interrupt(INT_ID_USART1, USART1_IRQHandler), UART_IT_RXNE is the trigger condiction.
   __HAL_UART_ENABLE(&uart1_handle); //after setting all parameter, enable this uart module.
-  
-
 }
 
 
 
 
-
-void USART1_IRQHandler() {
-	OS_ENTER_CRITICAL();                         /* Tell uC/OS-II that we are starting an ISR          */
-	OSIntEnter();
-
-	if (__HAL_UART_GET_IT(&UartHandleCAN, UART_IT_RXNE) != RESET) {
-		u16 data;
-		data = LL_LPUART_ReceiveData8(UartHandleCAN.Instance);
-		//do something.
-		
-	} else {
-		//finish transmitting or there is some error occurs in uart module.
-		LL_USART_ClearFlag_ORE(UartHandleCAN.Instance);
-		LL_LPUART_ReceiveData8(UartHandleCAN.Instance);	
+void USART1_IRQHandler(void) {
+	if(__HAL_UART_GET_FLAG(&UartHandle, UART_FLAG_RXNE)) { // Check if the RXNE flag is set (Receive data register not empty)
+		HAL_UART_RxCpltCallback(&UartHandle);// Call the RX Complete callback function
+		__HAL_UART_CLEAR_FLAG(&UartHandle, UART_CLEAR_RXNEF); // Clear the RXNE flag
 	}
+	
+	if(__HAL_UART_GET_FLAG(&UartHandle, UART_FLAG_TC)) {// Check if the TC flag is set (Transmission complete)
+		HAL_UART_TxCpltCallback(&UartHandle); // Call the TX Complete callback function
+		__HAL_UART_CLEAR_FLAG(&UartHandle, UART_CLEAR_TCF);// Clear the TC flag
+	}
+	// Other UART interrupt handling code if needed
+}
 
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	u16 data;
+	data = LL_LPUART_ReceiveData8(UartHandleCAN.Instance);
+	//do something. 
+}
 
-	OS_EXIT_CRITICAL();
-	OSIntExit();             /* Tell uC/OS-II that we are leaving the ISR */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+  // Handle the transmission complete event
+  // ...
+}
+
+void MySendBlocking() {
+	UART_HandleTypeDef UartHandle; // 你的UART句柄
+	uint8_t dataToSend[] = "Hello, UART!"; // 要傳送的數據
+	uint16_t sizeOfData = sizeof(dataToSend); // 要傳送的數據的大小
+	// 傳送數據
+	if(HAL_UART_Transmit(&UartHandle, dataToSend, sizeOfData, 1000) != HAL_OK) {
+	  Error_Handler();
+	} else {
+	  // 傳送成功
+	}
 }
 
 
+void MySendNoBlocking() {
+	UART_HandleTypeDef UartHandle; // 你的UART句柄
+	uint8_t dataToSend[] = "Hello, UART!"; // 要傳送的數據
+	uint16_t sizeOfData = sizeof(dataToSend); // 要傳送的數據的大小
+	if(HAL_UART_Transmit_IT(&UartHandle, dataToSend, sizeOfData) != HAL_OK) {
+	  Error_Handler();
+	} else {
+	  // 傳送已開始. after transmit complete, it will should trigger the HAL_UART_TxCpltCallback()
+	}
+}
 
